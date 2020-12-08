724d8d0e8e737bd22d20b1ee1c49d0a655b5af61
H8er <57931926+H8ER-ru@users.noreply.github.com>
2020-12-08 10:46:25 +0300

    modal windows

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
-console.log('file 1');
+// function modal(openBtn, block, closeBtn,
+//                typeOfBlock='block') {
+//     const open = document.querySelector(openBtn)
+//     const content = document.querySelector(block)
+//     const close = document.querySelector(closeBtn)
+//
+//     open.addEventListener('click', ()=> {
+//         content.style.display = typeOfBlock
+//     })
+//     close.addEventListener('click', () => {
+//         content.style.display = 'none'
+//     })
+// }
+//
+// modal('.js-openModalWriteMe', '.modal',
+//     '.js-modal__close', 'flex')
